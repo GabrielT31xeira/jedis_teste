6192dbc86ec52986f102e072ce93637ff147c445
Gabriel Teixeira de Carvalho <70416214+GabrielT31xeira@users.noreply.github.com>
2024-01-25 10:29:25 -0300

    Adicionando comentários as funções de login

--- app/Http/Controllers/api/AuthController.php
+++ app/Http/Controllers/api/AuthController.php
@@ -12,23 +12,29 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
+    # Função de login
     public function login(Request $request) : JsonResponse
     {
         try {
+            # Validação dos dados enviados
             $request->validate([
                 'email' => 'required|string|email',
                 'password' => 'required|string',
             ]);
 
             $data = $request->all();
-
+            # Verificando se os dados existem no banco de dados
             if (Auth::attempt($data)) {
+                # Pegando o usuario autenticado
                 $user = Auth::user();
+                # Gerando e retornando o token para o usuarios
                 $token = $user->createToken('JedisAPI')->accessToken;
                 return response()->json(['token' => $token], 200);
             } else {
+                # Caso os dados não sejam encontrados retorna não autorizado
                 return response()->json(['error' => 'Unauthorised'], 401);
             }
+        # Retorno de erros relacionados ao servidor
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'An error has occurred',
@@ -37,27 +43,31 @@ class AuthController extends Controller
         }
     }
 
+    # Cadastro de usuario
     public function register(Request $request)
     {
         try {
+            # Validação dos dados enviados
             $request->validate([
                 'name' => 'required|string|max:255',
                 'email' => 'required|string|email|max:255|unique:users',
                 'password' => 'required|string|min:6'
             ]);
 
+            # Criação do usuario de acordo com os dados enviados
             $user = User::create([
                 'name' => $request->name,
                 'email' => $request->email,
                 'password' => Hash::make($request->password),
             ]);
 
+            # Gerando e retornando o token para o usuarios
             $token = $user->createToken('LaravelAuthApp')->accessToken;
-
             return response()->json([
                 'message' => 'User created successfully',
-                'token' => $token->token
+                'token' => $token
             ]);
+        # Retorno de erros relacionados ao servidor
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'An error has occurred',
@@ -69,18 +79,26 @@ class AuthController extends Controller
     public function logout()
     {
         try {
+            # Checa a existencia do token
             if(Auth::guard('api')->check()){
+                # Busca o token no banco de dados
                 $accessToken = Auth::guard('api')->user()->token();
-
+                    # Revoga o token
                     DB::table('oauth_refresh_tokens')
                         ->where('access_token_id', $accessToken->id)
                         ->update(['revoked' => true]);
                 $accessToken->revoke();
-
+                #retorna mensagem de sucesso
                 return response()->json([
                     'message' => 'User logout successfully.'
                 ],200);
+            } else {
+                # Caso o token não seja encontrado
+                return response()->json([
+                    'message' => 'Token não encontrado'
+                ],404);
             }
+        # Retorno de erros relacionados ao servidor
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'An error has occurred',
@@ -91,10 +109,12 @@ class AuthController extends Controller
 
     public function profile() {
         try {
+            # Busca os dados do usuario logado
             $user = Auth::guard('api')->user();
             return response()->json([
                 'user' => $user
             ],200);
+        # Retorno de erros relacionados ao servidor
         } catch (\Exception $e) {
             return response()->json([
                 'message' => 'An error has occurred',
--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+# Rotas relacionadas ao login
+Route::post('login', [AuthController::class, 'login'])->name('login');
+Route::post('register', [AuthController::class, 'register']);
+Route::group(['middleware' => 'auth:api'], function () {
+    Route::post('logout', [AuthController::class, 'logout']);
+    Route::get('profile', [AuthController::class, 'profile']);
 });
-
- Route::post('login', [AuthController::class, 'login'])->name('login');
- Route::post('register', [AuthController::class, 'register']);
-
- Route::group(['middleware' => 'auth:api'], function () {
-     Route::post('logout', [AuthController::class, 'logout']);
-     Route::get('profile', [AuthController::class, 'profile']);
- });
